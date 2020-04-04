c8fb966deff014e3974a2ade16466439e20d1d06
aaljamid <aaljamid@emich.edu>
2020-04-05 00:13:37 +0300

    add create to requests controller

--- salvation-request/app/controllers/requests_controller.rb
+++ salvation-request/app/controllers/requests_controller.rb
@@ -12,4 +12,16 @@ class RequestsController < ApplicationController
     def new
         @request = Request.new
     end
+
+    def create 
+        @request = Request.new(request_params)
+        @request.save
+
+         redirect_to @request
+    end
+
+    private
+    def request_params
+         params.require(:request).permit(:first_name, :last_name, :street, :city, :zip_code, :phone)
+    end
 end
