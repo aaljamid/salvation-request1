75a2c63fad29cd6a03cc8a035513fc527d2c7346
aaljamid <aaljamid@emich.edu>
2020-04-04 23:57:38 +0300

    add show, new to requests controller

--- salvation-request/app/controllers/requests_controller.rb
+++ salvation-request/app/controllers/requests_controller.rb
@@ -4,4 +4,12 @@ class RequestsController < ApplicationController
         @requests = Request.all
 
     end
+
+    def show
+        @request = Request.find(params[:id])
+    end
+
+    def new
+        @request = Request.new
+    end
 end
