e7fd71aedd99d68c518a1a3bcdd746749900e505
aaljamid <aaljamid@emich.edu>
2020-04-06 00:46:31 +0300

    add index to Requests controller

--- salvation-request/app/controllers/requests_controller.rb
+++ salvation-request/app/controllers/requests_controller.rb
@@ -1,2 +1,9 @@
 class RequestsController < ApplicationController
+
+    def index
+        @name = "User 01"
+        @requests = Request.all
+    #    @requests = current_user.requests.all
+    end
+
 end
--- salvation-request/db/seeds.rb
+++ salvation-request/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Request.create(request_type:"Food",request_description:" User 1",request_status:"New")
