3060c81ba97ada4df0cdfd678911a2181917cfe8
aaljamid <aaljamid@emich.edu>
2020-04-06 00:58:30 +0300

    add new and create to Requests controller

--- salvation-request/app/controllers/requests_controller.rb
+++ salvation-request/app/controllers/requests_controller.rb
@@ -10,4 +10,20 @@ class RequestsController < ApplicationController
         @request = Request.find(params[:id])
     end
 
+
+    def new
+        @request = Request.new
+    end
+
+    def create 
+        @request = Request.new(request_params)
+        @request.save
+
+         redirect_to @request
+    end
+
+    private
+    def request_params
+         params.require(:request).permit(:request_type, :request_description, :request_status)
+    end
 end
