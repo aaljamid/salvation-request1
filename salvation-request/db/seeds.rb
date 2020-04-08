17bff9da9a2b9dbbdab59ea1e87d37797cbba804
aaljamid <aaljamid@emich.edu>
2020-04-08 15:47:18 +0300

    remove seeds

--- salvation-request/db/seeds.rb
+++ salvation-request/db/seeds.rb
@@ -6,4 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Request.create(request_type:"Food",request_description:" User 1",request_status:"New")
